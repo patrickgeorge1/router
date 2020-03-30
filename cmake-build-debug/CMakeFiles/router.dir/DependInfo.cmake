9247fff564e0f725294f327e8bde52aebeedc560
patrickgeorge1 <patrionpatrick@gmail.com>
2020-03-30 20:51:37 +0300

    skel

---
+++ cmake-build-debug/CMakeFiles/router.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/patrickgeorge1/Desktop/router/list.c" "/home/patrickgeorge1/Desktop/router/cmake-build-debug/CMakeFiles/router.dir/list.c.o"
+  "/home/patrickgeorge1/Desktop/router/queue.c" "/home/patrickgeorge1/Desktop/router/cmake-build-debug/CMakeFiles/router.dir/queue.c.o"
+  "/home/patrickgeorge1/Desktop/router/router.c" "/home/patrickgeorge1/Desktop/router/cmake-build-debug/CMakeFiles/router.dir/router.c.o"
+  "/home/patrickgeorge1/Desktop/router/skel.c" "/home/patrickgeorge1/Desktop/router/cmake-build-debug/CMakeFiles/router.dir/skel.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/router.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/router.dir/list.c.o"
+  "CMakeFiles/router.dir/queue.c.o"
+  "CMakeFiles/router.dir/router.c.o"
+  "CMakeFiles/router.dir/skel.c.o"
+  "router"
+  "router.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/router.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
