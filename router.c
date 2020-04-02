a37e52dea8018fe1942a5f4f7f85c60016dcb76b
patrickgeorge1 <patrionpatrick@gmail.com>
2020-04-02 23:40:55 +0300

    90 , nu merge forward 02

--- router.c
+++ router.c
@@ -180,13 +180,75 @@ void process_arp_reply(packet m, struct arp_vector *arp_table, queue q) {
     }
 }
 
+uint16_t ip_checksum(void* vdata,size_t length) {
+    // Cast the data pointer to one that can be indexed.
+    char* data=(char*)vdata;
+
+    // Initialise the accumulator.
+    uint64_t acc=0xffff;
+
+    // Handle any partial block at the start of the data.
+    unsigned int offset=((uintptr_t)data)&3;
+    if (offset) {
+        size_t count=4-offset;
+        if (count>length) count=length;
+        uint32_t word=0;
+        memcpy(offset+(char*)&word,data,count);
+        acc+=ntohl(word);
+        data+=count;
+        length-=count;
+    }
+
+    // Handle any complete 32-bit blocks.
+    char* data_end=data+(length&~3);
+    while (data!=data_end) {
+        uint32_t word;
+        memcpy(&word,data,4);
+        acc+=ntohl(word);
+        data+=4;
+    }
+    length&=3;
+
+    // Handle any partial block at the end of the data.
+    if (length) {
+        uint32_t word=0;
+        memcpy(&word,data,length);
+        acc+=ntohl(word);
+    }
+
+    // Handle deferred carries.
+    acc=(acc&0xffffffff)+(acc>>32);
+    while (acc>>16) {
+        acc=(acc&0xffff)+(acc>>16);
+    }
+
+    // If the data began at an odd byte address
+    // then reverse the byte order to compensate.
+    if (offset&1) {
+        acc=((acc&0xff00)>>8)|((acc&0x00ff)<<8);
+    }
+
+    // Return the checksum in network byte order.
+    return htons(~acc);
+}
+
+
 void process_ip(packet m, struct arp_vector *arp_table, struct route_element* routing_table,  queue q){
     struct ether_header *ethernet = (struct ether_header *)m.payload;
     struct ip_hdr *ip = (struct ip_hdr *)(m.payload + IP_OFF);
 
+    __u16 control_sum =  ip->csum;
+    ip->csum = 0;
+    if (control_sum != ip_checksum(ip, sizeof(struct ip_hdr))) {
+        return;
+    }
 
     struct route_element * next_hop = get_best_route(routing_table, *((uint32_t*)ip->daddr));  // TODO check uint_8 to uint_32 conversion
-    if (next_hop != NULL) {
+    if (ip->ttl >= 1 && next_hop != NULL) {
+        ip->ttl--;
+        ip->csum = ip_checksum(ip, sizeof(struct ip_hdr));
+
+
         if (search_arp(arp_table, ip->daddr) != -1) {   // daca am mac trimit direct
 
             int index = search_arp(arp_table, ip->daddr);
@@ -244,6 +306,7 @@ void process_ip(packet m, struct arp_vector *arp_table, struct route_element* ro
         }
     }
 }
+
 
 int main(int argc, char *argv[])
 {
